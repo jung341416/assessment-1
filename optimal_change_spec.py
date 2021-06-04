fae3cd07485b6f251d3bbb9b4e6a51ede7baccf9
jung341416 <jung341416@gmail.com>
2021-06-04 12:32:42 -0400

    first commit

--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,10 @@
 # Write your unit tests here!
+import unittest
+from optimal_change import optimal_change
+
+class OptimalChangeTestCase(unittest.TestCase):
+    def test_optimal_change(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
