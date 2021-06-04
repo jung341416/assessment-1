95c7886fb2936ebf9c209468aacc4d74bb1a1e94
jung341416 <jung341416@gmail.com>
2021-06-04 12:44:02 -0400

    pseudo code

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,12 @@
 # Write your solution here!
+# creating function that takes two arguments:
+def optimal_change(item_cost, amount_paid):
+    pass
+#Pseducode:
+#create result string
+#create change variable
+#create the list of dictionary which will store bills data
+#create variable index which will track how many each bills needed
+#print result string
+
+    
