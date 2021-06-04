53a964073baccdbee8d6d0263dbf308c737971b7
jung341416 <jung341416@gmail.com>
2021-06-04 12:59:57 -0400

    initial test cases completed

--- optimal_change.py
+++ optimal_change.py
@@ -1,12 +1,25 @@
 # Write your solution here!
 # creating function that takes two arguments:
 def optimal_change(item_cost, amount_paid):
-    pass
+
 #Pseducode:
 #create result string
+    result_str = ""
 #create change variable
+    change = amount_paid - item_cost
 #create the list of dictionary which will store bills data
+    bill_data = [{
+        "$20" : 20,
+        "$10" : 10,
+        "$5" : 5,
+        "$1" : 1,
+        "quarter" : 0.25,
+        "dime" : 0.10,
+        "Nickle" : 0.05,
+        "penny" : 0.01
+    }]
 #create variable index which will track how many each bills needed
 #print result string
-
+    print(result_str)
+pass
     
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -4,7 +4,15 @@ from optimal_change import optimal_change
 
 class OptimalChangeTestCase(unittest.TestCase):
     def test_optimal_change(self):
-        pass
+        actual_output1 = optimal_change(62.13, 100)
+        expected_output1 = "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies."
+        #####################################################################
+        actual_output2 = optimal_change(62.13, 100)
+        expected_output2 = "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies."
+        #####################################################################
+        self.assertEqual(actual_output1, expected_output1)
+        self.assertEqual(actual_output2, expected_output2)
+    
 
 if __name__ == "__main__":
     unittest.main()
