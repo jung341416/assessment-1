fd3ec9e76479a64e253e5e9500931e4e20ac8304
jung341416 <jung341416@gmail.com>
2021-07-10 04:49:21 -0400

    assessment one retake

--- optimal_change.py
+++ optimal_change.py
@@ -1,60 +1,53 @@
-# Write your solution here!
-# creating function that takes two arguments:
+# write a function that takes two arguments item_cost, amount_paid
 def optimal_change(item_cost, amount_paid):
-
-#Pseducode:
-#create result string
-    result_str_one = f"The optimal change for an item that costs ${item_cost} with an amount paid of ${amount_paid} is"
-    result_str_two = ""
-#create the list of dictionary which will store bills data
-    bill_data_one = [
-        {"$20" : 20},
-        {"$10" : 10},
-        {"$5" : 5},
-        {"$1" : 1},
-        ]
-    bill_data_two =  [
-        {"quarter" : 25},
-        {"dime" : 10},
-        {"Nickle" : 5},
-        {"penny" : 1},
-        ]
-# create variable index which will track how many each bills needed
-# iterate throught the list and get the key and number value and store it variables
-#create change variable
-    change = amount_paid - item_cost
-    change = str(change).split('.')
-    change_one = int(change[0])
-    change_two = int(change[1])
-    
-    for dic in bill_data_one:
-        for key in dic:
-            bill_str = key
-            bill_int = dic[key]
-            count = int(change_one // bill_int) 
-            countPlural = f"{'bills' if count > 1 else 'bill'}"
-            if change_one >= bill_int:
-                change_one = change_one - (bill_int * count)                
-                result_str_one += f" {count} {bill_str} {countPlural},"
-    
-    for dic in bill_data_two:
-        for key in dic:
-            bill_str = key
-            bill_int = dic[key]
-            count = int(change_two // bill_int)
-            plural = f"{'s' if count > 1 else ''}"
-            if change_two > bill_int:
-                change_two = change_two - (bill_int * count)
-                if(key == "penny"):
-                    result_str_two += f" and {count} pennies."
+    #declare template string which will be the first half of expected output string
+    result_string_template = f"The optimal change for an item that costs ${item_cost} with an amount paid of ${amount_paid} is"
+    #declare empty string which will go through loop and be the added to template string
+    result_string_to_be_added = ""
+    #declare change variable and round it up 2 decimals
+    change = amount_paid -item_cost
+    #make list of dictionaries for money string and money number for calculation
+    moneys = [
+        {"$20":20}, 
+        {"$10":10} ,
+        {"$5" :5}, 
+        {"$1" :1}, 
+        {"quarter": 0.25}, 
+        {"dime": 0.1}, 
+        {"nickle": 0.05}, 
+        {"penny": 0.01}]
+    #loop counter    
+    loop_count = 0
+    #make for loop 
+    for index, money_dic in enumerate(moneys):
+        for key in money_dic:
+            #number value in money dictionary which will use for calculation
+            money_amt = money_dic[key]
+            #make while loop that run until the change is less than or equal to money to be handed
+            while change >= money_amt:
+                # make variable that will count amounts that need for each bill
+                money_count = int(change // money_amt)
+                change = round(change - (money_amt * money_count),2)
+                loop_count = loop_count + 1
+                #if money counnt is greater than 1 need plurals for each bill coin and penny cases 
+                bill_count_plural_check = f"{' bills' if money_count > 1 else ' bill'}"
+                coin_count_plural_check = f"{'s' if money_count > 1 else ''}"
+                penny_plural_check = f"{'pennies' if key == 'penny' and money_count > 1 else 'penny'}"
+                bill_or_coin_plural_check = bill_count_plural_check if index < 4 else coin_count_plural_check
+                no_more_change = change == 0
+                # change went only one loop then no need to add and still need to check coin or bill plural
+                    # morethan one loop change end in coin then and comes before the end still need to check coin or bill plural
+                    # change end in bill then and comes before the end still need to check coin or bill plural
+                if key == 'penny' and loop_count == 1:
+                    result_string_to_be_added += f" {money_count} {penny_plural_check}."
+                elif no_more_change and loop_count == 1:
+                    result_string_to_be_added += f" {money_count} {key}{bill_or_coin_plural_check}."
+                elif not no_more_change:
+                    result_string_to_be_added += f" {money_count} {key}{bill_or_coin_plural_check},"
+                elif no_more_change and key != 'penny':
+                    result_string_to_be_added += f" and {money_count} {key}{bill_or_coin_plural_check}."
                 else:
-                    result_str_two += f" {count} {bill_str}{plural},"            
-            
-
-#print result string
-    if change_two != None:
-        print(result_str_one + result_str_two) 
-    else: 
-        print(result_str_one)
-
-
+                    result_string_to_be_added += f" and {money_count} {penny_plural_check}."
+        
+               
+    return result_string_template + result_string_to_be_added
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -6,25 +6,34 @@ class OptimalChangeTestCase(unittest.TestCase):
     def test_optimal_change(self):
         actual_output1 = optimal_change(62.13, 100)
         expected_output1 = "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies."
-        #####################################################################
+        ##################################################################################################################################################################
         actual_output2 = optimal_change(31.51, 50)
         expected_output2 = "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies."
-        #####################################################################
+        ##################################################################################################################################################################
         actual_output3 = optimal_change(92.25, 100)
         expected_output3 = "The optimal change for an item that costs $92.25 with an amount paid of $100 is 1 $5 bill, 2 $1 bills, and 3 quarters."
-        #####################################################################
-        actual_output4 = optimal_change(91.50, 100)
-        expected_output4 = "The optimal change for an item that costs $91.50 with an amount paid of $100 is 1 $5 bill, 3 $1 bills, and 2 quarters."
-        #####################################################################
-        # actual_output5 = optimal_change(62.13, 100)
-        # expected_output5 = "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies."
-        #####################################################################
+        ##################################################################################################################################################################
+        actual_output4 = optimal_change(91.5, 100)
+        expected_output4 = "The optimal change for an item that costs $91.5 with an amount paid of $100 is 1 $5 bill, 3 $1 bills, and 2 quarters."
+        ##################################################################################################################################################################
+        actual_output5 = optimal_change(62.13, 100)
+        expected_output5 = "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies."
+        ##################################################################################################################################################################
+        actual_output6 = optimal_change(0.1, 1)
+        expected_output6 = "The optimal change for an item that costs $0.1 with an amount paid of $1 is 3 quarters, 1 dime, and 1 nickle."
+        ##################################################################################################################################################################
+        actual_output7 = optimal_change(20, 100)
+        expected_output7 = "The optimal change for an item that costs $20 with an amount paid of $100 is 4 $20 bills."
+        actual_output8 = optimal_change(0.05, 0.1)
+        expected_output8 = "The optimal change for an item that costs $0.05 with an amount paid of $0.1 is 1 nickle."
         self.assertEqual(actual_output1, expected_output1)
         self.assertEqual(actual_output2, expected_output2)
         self.assertEqual(actual_output3, expected_output3)
         self.assertEqual(actual_output4, expected_output4)
-        # self.assertEqual(actual_output5, expected_output5)
-    
+        self.assertEqual(actual_output5, expected_output5)
+        self.assertEqual(actual_output6, expected_output6)
+        self.assertEqual(actual_output7, expected_output7)
+        self.assertEqual(actual_output8, expected_output8)
 
 if __name__ == "__main__":
     unittest.main()
